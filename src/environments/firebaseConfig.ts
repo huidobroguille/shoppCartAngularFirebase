022dcea3367480703225c6cf3d4c5bce89ea0134
guillermo <ghxghxgh@gmail.com>
2019-10-17 14:50:36 -0300

    primer commit

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyC85bed3PIqnBKD400d7LqAByEnah0tLn4",
+  authDomain: "shoppingangular-ac8d6.firebaseapp.com",
+  databaseURL: "https://shoppingangular-ac8d6.firebaseio.com",
+  projectId: "shoppingangular-ac8d6",
+  storageBucket: "shoppingangular-ac8d6.appspot.com",
+  messagingSenderId: "108340973140",
+  appId: "1:108340973140:web:53d4c3d7384c47128da7c1",
+  measurementId: "G-8R9V4F8ZG0"
+};
